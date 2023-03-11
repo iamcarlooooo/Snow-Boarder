b2e432ae46b0e04c1f2b0f51e94e3360ccc75542
Carlo Alain Aquino <carlo.aquino412@gmail.com>
2023-03-12 02:23:08 +0800

    Finishes 7:42 of Lesson 36

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+
+
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] float torqueAmount = 1f;
+    Rigidbody2D rb2d;
+
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.AddTorque(torqueAmount);
+        } 
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.AddTorque(torqueAmount);
+        }
     }
 }
