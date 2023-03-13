4e3c4e416a0b6331ed76ab248229f0d6f3d4a773
Carlo Alain Aquino <carlo.aquino412@gmail.com>
2023-03-14 01:59:14 +0800

    Finishes Lesson 43

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -10,26 +11,47 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] float torqueAmount = 20.0f;
+    [SerializeField] float boostSpeed = 30.0f;
+    [SerializeField] float normalSpeed = 20.0f;
+
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2d;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2d = FindAnyObjectByType<SurfaceEffector2D>();
     }
 
 
     void Update()
     {
+        RotatePlayer();
+        RespondToBoost();
+
+    }
+
+    private void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2d.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2d.speed = normalSpeed;
+        }
+    }
+
+    private void RotatePlayer()
+    {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb2d.AddTorque(torqueAmount);
-        } 
+        }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             rb2d.AddTorque(-torqueAmount);
         }
-
-       
-
     }
 }
